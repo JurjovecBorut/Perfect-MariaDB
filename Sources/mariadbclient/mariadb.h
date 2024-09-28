e42902e998e56d4c7f0e05f9a32e9dfc263ddd2a
JurjovecBorut <162068264+JurjovecBorut@users.noreply.github.com>
2024-09-28 09:55:26 +0200

    Update mariadb.h

--- Sources/mariadbclient/mariadb.h
+++ Sources/mariadbclient/mariadb.h
@@ -1,7 +1,8 @@
 #ifndef __PERFECT_MARIADB__
 #define __PERFECT_MARIADB__
 #ifdef __APPLE__
-#include "/usr/local/include/mariadb/mysql.h"
+//#include "/usr/local/include/mariadb/mysql.h"
+#include "/usr/local/Cellar/mariadb/11.5.2/include/mysql/mysql.h"
 #else
 #include "/usr/include/mariadb/mysql.h"
 #endif
